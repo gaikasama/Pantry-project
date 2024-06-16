f8b243ee642611c298eabfbb74c41825eec3786c
tyanpey <tyanpey@gmail.com>
2024-06-16 20:15:13 +0900

    Created modal for item add

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -5,81 +5,63 @@ import {
   Flex as FlexUM,
   Text as TextUM,
 } from "@tyanpey/urban_memories_lib";
-import { Dialog, TextField, Button, Flex, Text } from "@radix-ui/themes";
-import { useState } from "react";
 
 import { useItems } from "@/Contexts/ItemsContext/ItemsWrapper";
+import { useAppModal } from "@/Contexts/AppModalContext/AppModalWrapper";
+import { ItemEditModal } from "./_components/ItemEditModal";
 
 export default function ItemsPage() {
   const { items, setItems } = useItems();
-  const [newItem, setNewItem] = useState("");
+  const modal = useAppModal();
 
   const handleAddItem = () => {
-    const newItemObj = {
-      id: items.length + 1,
-      name: newItem,
-    };
-
-    setItems([...items, newItemObj]);
-    setNewItem("");
+    modal.openModal(
+      {
+        title: "Add Item",
+        icon: undefined,
+      },
+      <ItemEditModal
+        closeModal={() => {
+          modal.closeModal();
+        }}
+      />
+    );
   };
 
   const handleDeleteItem = (itemId: number) => {
+    if (!confirm("Are you sure you want to delete this item?")) return;
     const updatedItems = items.filter((item) => item.id !== itemId);
     setItems(updatedItems);
   };
 
   return (
-    <FlexUM direction="v">
-      {items.map((item) => {
-        return (
-          <Card key={item.id}>
-            <Flex justify={"between"}>
-              <TextUM text={item.name} bold />
-              <ButtonUM
-                text="Delete Item"
-                mode={"bgGray"}
-                fullWidth={false}
-                onClick={() => handleDeleteItem(item.id)}
-              />
-            </Flex>
-          </Card>
-        );
-      })}
-      <Dialog.Root>
-        <Dialog.Trigger>
-          <ButtonUM text="Add Item" mode={"bgGray"} fullWidth />
-        </Dialog.Trigger>
+    <FlexUM direction="v" gap="20px">
+      {/* List */}
+      <FlexUM direction="v">
+        {items.map((item) => {
+          return (
+            <Card key={item.id}>
+              <FlexUM horizontally="apart">
+                <TextUM text={item.name} bold />
+                <ButtonUM
+                  text="Delete Item"
+                  mode={"bgGray"}
+                  fullWidth={false}
+                  onClick={() => handleDeleteItem(item.id)}
+                />
+              </FlexUM>
+            </Card>
+          );
+        })}
+      </FlexUM>
 
-        <Dialog.Content maxWidth="450px">
-          <Dialog.Title>Add items</Dialog.Title>
-
-          <Flex direction="column" gap="3">
-            <label>
-              <Text as="div" size="2" mb="1" weight="bold">
-                Name
-              </Text>
-              <TextField.Root
-                value={newItem}
-                onChange={(e) => setNewItem(e.target.value)}
-                defaultValue=""
-                placeholder="Enter item"
-              />
-            </label>
-          </Flex>
-
-          <Flex gap="3" mt="4" justify="end">
-            <Dialog.Close>
-              <Button variant="soft" color="gray">
-                Cancel
-              </Button>
-            </Dialog.Close>
-            <Dialog.Close>
-              <Button onClick={handleAddItem}>Save</Button>
-            </Dialog.Close>
-          </Flex>
-        </Dialog.Content>
-      </Dialog.Root>
+      {/* Add Button */}
+      <ButtonUM
+        text="Add Item"
+        onClick={handleAddItem}
+        mode={"bgGray"}
+        fullWidth
+      />
     </FlexUM>
   );
 }
