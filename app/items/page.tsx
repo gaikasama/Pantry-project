023eda8e02c92b3b9026247a12f7aafafd9908d8
gaikasama <gaikagaiko666@gmail.com>
2024-06-16 16:35:06 +0900

    items add

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,26 +1,72 @@
 "use client";
-import { Button, Card, Flex, SlideupModal, Text } from "@tyanpey/urban_memories_lib";
-import { Item, itemsData } from "../../Contexts/ItemsContext/data";
+import {
+  Button as ButtonUM,
+  Card,
+  Flex as FlexUM,
+  Text as TextUM,
+} from "@tyanpey/urban_memories_lib";
+import { Dialog, TextField, Button, Flex, Text } from "@radix-ui/themes";
 import { useState } from "react";
 
-export default function ItemsPage() {
+import { useItems } from "@/Contexts/ItemsContext/ItemsWrapper";
 
-  const [items, setItems] = useState<Item[]>(itemsData);
+export default function ItemsPage() {
+  const { items, setItems } = useItems();
+  const [newItem, setNewItem] = useState("");
 
   const handleAddItem = () => {
-    alert("function to add item.");
-  }
+    const newItemObj = {
+      id: items.length + 1,
+      name: newItem,
+    };
+
+    setItems([...items, newItemObj]);
+    setNewItem("");
+  };
 
   return (
-    <Flex direction="v">
+    <FlexUM direction="v">
       {items.map((item) => {
         return (
           <Card key={item.id}>
-            <Text text={item.name} bold />
+            <TextUM text={item.name} bold />
           </Card>
         );
       })}
-      <Button text="Add Item" mode={"bgGray"} fullWidth onClick={handleAddItem} />
-    </Flex>
+      <Dialog.Root>
+        <Dialog.Trigger>
+          <ButtonUM text="Add Item" mode={"bgGray"} fullWidth />
+        </Dialog.Trigger>
+
+        <Dialog.Content maxWidth="450px">
+          <Dialog.Title>Add items</Dialog.Title>
+
+          <Flex direction="column" gap="3">
+            <label>
+              <Text as="div" size="2" mb="1" weight="bold">
+                Name
+              </Text>
+              <TextField.Root
+                value={newItem}
+                onChange={(e) => setNewItem(e.target.value)}
+                defaultValue=""
+                placeholder="Enter item"
+              />
+            </label>
+          </Flex>
+
+          <Flex gap="3" mt="4" justify="end">
+            <Dialog.Close>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </Dialog.Close>
+            <Dialog.Close>
+              <Button onClick={handleAddItem}>Save</Button>
+            </Dialog.Close>
+          </Flex>
+        </Dialog.Content>
+      </Dialog.Root>
+    </FlexUM>
   );
 }
