be0c1c5279266cc90a289e0df8ca6d5632d85f1e
tyanpey <tyanpey@gmail.com>
2024-06-18 20:07:31 +0900

    type definition and placeholder data for Stock.

--- Contexts/ItemsContext/data.ts
+++ Contexts/ItemsContext/data.ts
@@ -1,4 +1,6 @@
 
+
+// --------- Item ------------------
 export type Item = {
   id: number;
   name: string;
@@ -8,4 +10,20 @@ export const itemsData: Item[] = [
   { id: 1, name: "item 1" },
   { id: 2, name: "item 2" },
   { id: 3, name: "item 3" },
+]
+
+// --------- Stock ------------------
+export type Stock = {
+  id: number;
+  itemId: number;
+  expiryDate?: Date;
+}
+
+export const stocksData: Stock[] = [
+  { id: 1, itemId: 1, expiryDate: new Date() },
+  { id: 2, itemId: 1, expiryDate: new Date(new Date().getDate() + 2) },
+  { id: 3, itemId: 2 },
+  { id: 4, itemId: 3 },
+  { id: 5, itemId: 3 },
+  { id: 6, itemId: 3 },
 ]
